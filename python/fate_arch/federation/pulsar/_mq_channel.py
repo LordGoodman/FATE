5e175e147739372bd652c816027374180b58f790
jiahaoc <jiahaoc@vmware.com>
2021-02-28 10:47:48 +0000

    update the creation of producer and consumer
    Signed-off-by: jiahaoc <jiahaoc@vmware.com>

--- python/fate_arch/federation/pulsar/_mq_channel.py
+++ python/fate_arch/federation/pulsar/_mq_channel.py
@@ -56,16 +56,10 @@ class MQChannel(object):
         self._role = role
         self._extra_args = extra_args
 
-        # "_conn" is a "client" object that handles pulsar connection
-        self._conn = None
         # "_channel" is the subscriptor for the topic
         self._producer_send = None
-        self._consumer_send = None
 
-        self._producer_receive = None
         self._consumer_receive = None
-
-        self._sequence_id = None
 
         self._producer_config = {}
         if extra_args.get('producer') is not None:
@@ -81,33 +75,75 @@ class MQChannel(object):
 
     @connection_retry
     def basic_publish(self, body, properties):
-        self._get_channel()
+        self._get_or_create_producer()
         LOGGER.debug('send queue: {}'.format(
             self._producer_send.topic()))
         LOGGER.debug(f"send data: {body}")
-        self._producer_send.send(content=body, properties=properties)
-        self._sequence_id = self._producer_send.last_sequence_id()
+
+        try:
+            self._producer_send.send(content=body, properties=properties)
+        except Exception as e:
+            self._producer_send = None
+            # propogate exception to upper level
+            raise e
 
     @connection_retry
     def consume(self):
-        self._get_channel()
-        # since consumer and topic are one to one corresponding, maybe it is ok to use unique subscription name?
+        self._get_or_create_consumer()
+
         LOGGER.debug('receive topic: {}'.format(
             self._consumer_receive.topic()))
-        message = self._consumer_receive.receive()
-        return message
+
+        try:
+            message = self._consumer_receive.receive()
+            return message
+        except Exception as e:
+            self._consumer_receive = None
+            raise e
 
     @connection_retry
     def basic_ack(self, message):
-        self._get_channel()
-        return self._consumer_receive.acknowledge(message)
+        self._get_or_create_consumer()
+        try:
+            self._consumer_receive.acknowledge(message)
+            return
+        except Exception as e:
+            self._consumer_receive = None
+            raise e
 
     @connection_retry
     def cancel(self):
-        self._get_channel()
-        return self._consumer_receive.close()
+        # self._get_or_create_consumer()
+        # self._get_or_create_producer()
+        self._consumer_receive.close()
+        self._producer_send.close()
 
-    @connection_retry
+    def _get_or_create_producer(self):
+        if self._producer_send is None:
+            connection = pulsar.Client(
+                'pulsar://{}:{}'.format(self._host, self._port))
+
+            # TODO: it is little bit dangerous to pass _extra_args here ;)
+            # TODO: find a batter way to avoid pairs
+            self._producer_send = connection.create_producer(TOPIC_PREFIX.format(self._namespace, self._send_topic),
+                                                             producer_name=UNIQUE_PRODUCER_NAME,
+                                                             message_routing_mode=_pulsar.PartitionsRoutingMode.UseSinglePartition,
+                                                             # initial_sequence_id=self._sequence_id,
+                                                             **self._producer_config)
+
+    def _get_or_create_consumer(self):
+        if self._consumer_receive is None:
+            connection = pulsar.Client(
+                'pulsar://{}:{}'.format(self._host, self._port))
+
+            # TODO: it is little bit dangerous to pass _extra_args here ;)
+            # TODO: find a batter way to avoid pairs
+            self._consumer_receive = connection.subscribe(TOPIC_PREFIX.format(self._namespace, self._receive_topic),
+                                                          subscription_name=DEFAULT_SUBSCRIPTION_NAME,
+                                                          consumer_name=UNIQUE_CONSUMER_NAME,
+                                                          initial_position=_pulsar.InitialPosition.Earliest,
+                                                          **self._consumer_config)
+
     def _get_channel(self):
         self._clear()
         self._conn = pulsar.Client(
