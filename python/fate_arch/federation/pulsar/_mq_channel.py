334fbea5b31645add9d7aa234665d729b983417c
jiahaoc <jiahaoc@vmware.com>
2021-02-28 12:48:43 +0000

    add timeout to check alive
    Signed-off-by: jiahaoc <jiahaoc@vmware.com>

--- python/fate_arch/federation/pulsar/_mq_channel.py
+++ python/fate_arch/federation/pulsar/_mq_channel.py
@@ -157,7 +157,7 @@ class MQChannel(object):
     def _check_consumer_alive(self):
         try:
             self._consumer_conn.get_topic_partitions("test-alive")
-            message = self._consumer_receive.receive()
+            message = self._consumer_receive.receive(timeout_millis=500)
             self._consumer_receive.negative_acknowledge(message)
             # self._consumer_receive.acknowledge_cumulative(self._latest_confirmed)
             # self._consumer_receive.negative_acknowledge(message)
