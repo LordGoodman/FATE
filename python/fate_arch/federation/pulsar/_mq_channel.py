80da239a470957ac818c87b023f989771e39136c
jiahaoc <jiahaoc@vmware.com>
2021-03-01 03:50:05 +0000

    simpliy check

--- python/fate_arch/federation/pulsar/_mq_channel.py
+++ python/fate_arch/federation/pulsar/_mq_channel.py
@@ -124,23 +124,34 @@ class MQChannel(object):
 
     @connection_retry
     def _get_or_create_producer(self):
-        #if self._check_producer_alive() != True:
-        self._producer_conn = pulsar.Client(
-            service_url='pulsar://{}:{}'.format(self._host, self._port),
-            operation_timeout_seconds=3)
+        # if self._check_producer_alive() != True:
+        if self._producer_conn is None:
+            try:
+                self._producer_conn = pulsar.Client(
+                    service_url='pulsar://{}:{}'.format(
+                        self._host, self._port),
+                    operation_timeout_seconds=3)
+            except Exception:
+                self._producer_conn = None
 
+        # alway used current client to fetch producer
         self._producer_send = self._producer_conn.create_producer(TOPIC_PREFIX.format(self._namespace, self._send_topic),
                                                                   producer_name=UNIQUE_PRODUCER_NAME,
-                                                                  #send_timeout_millis=500,
-                                                                  #initial_sequence_id=self._sequence_id,
+                                                                  # send_timeout_millis=500,
+                                                                  # initial_sequence_id=self._sequence_id,
                                                                   **self._producer_config)
 
     @connection_retry
     def _get_or_create_consumer(self):
-        #if self._check_consumer_alive() != True:
-        self._consumer_conn = pulsar.Client(
-            service_url='pulsar://{}:{}'.format(self._host, self._port),
-            operation_timeout_seconds=3)
+        # if self._check_consumer_alive() != True:
+        if self._consumer_conn is None:
+            try:
+                self._consumer_conn = pulsar.Client(
+                    service_url='pulsar://{}:{}'.format(
+                        self._host, self._port),
+                    operation_timeout_seconds=3)
+            except Exception:
+                self._consumer_conn = None
 
         self._consumer_receive = self._consumer_conn.subscribe(TOPIC_PREFIX.format(self._namespace, self._receive_topic),
                                                                subscription_name=DEFAULT_SUBSCRIPTION_NAME,
